9f884d0fe9a96ba09404d7ca6045e1fac751332d
Joshua Gross <joshua.gross@gmail.com>
2020-12-17 21:34:39 -0800

    2020, day 18

---
+++ 2020/day18.2.js
@@ -0,0 +1,72 @@
+const fs = require('fs');
+const assert = require('assert');
+const sum = require('../utils/sum');
+
+function tokenize (str) {
+  const tokens = [];
+  for (let i = 0; i < str.length; i++) {
+    if (str[i].match(/[0-9]/)) {
+      let tmp = '';
+      while (str[i] && str[i].match(/[0-9]/)) {
+        tmp += str[i++];
+      }
+      tokens.push(parseInt(tmp, 10));
+    }
+    if (str[i] && str[i].match(/[\(\)\+\-\*\/]/)) {
+      tokens.push(str[i]);
+    }
+  }
+  return tokens;
+}
+
+function parseInput (str) {
+  return str.split('\n').filter(x => !!x).map(line => tokenize(line));
+}
+
+function evaluateNumber (expr, i = 0, level = 0) {
+  let initialValue;
+
+  if (typeof expr[i] === 'number') {
+    initialValue = expr[i++];
+  } else if (expr[i] === '(') {
+    [i, initialValue] = evaluateExpr(expr, i + 1, level + 1);
+  }
+
+  while (expr[i] === '+' || expr[i] === '-') {
+    const op = expr[i];
+    [i, nextNum] = evaluateNumber(expr, i+1, level);
+    initialValue += nextNum * (op === '-' ? -1 : 1);
+  }
+
+  return [i, initialValue];
+}
+
+function evaluateExpr (expr, i = 0, level = 0) {
+  let value;
+  [i, value] = evaluateNumber(expr, i, level);
+
+  while (expr[i] != null && expr[i] != ')') {
+    const op = expr[i++];
+    let num;
+    [i, num] = evaluateNumber(expr, i, level);
+
+    if (op === '*') {
+      value = value * num;
+    } else if (op === '/') {
+      value = value / num;
+    }
+  }
+
+  assert.equal(expr[i] == null || expr[i] === ')', true);
+  return [i + 1, value];
+}
+
+module.exports = function runner(inputFile, verbose) {
+  let input = parseInput(fs.readFileSync(inputFile, 'utf8'), 3);
+  const values = input.map(line => evaluateExpr(line)[1]);
+
+  const solution = sum(...values);
+
+  assert.equal(solution, 472171581333710);
+  console.log('Final Answer:', solution);
+}
